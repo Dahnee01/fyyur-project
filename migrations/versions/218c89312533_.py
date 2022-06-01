9d6addae6b5a9cfffa2e0a70f22247eb5ee2689b
daniel <mayodan21@gmail.com>
2022-06-01 07:11:47 +0100

    initial commit

---
+++ migrations/versions/218c89312533_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 218c89312533
+Revises: 74ae1f164251
+Create Date: 2022-05-30 14:53:01.360771
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '218c89312533'
+down_revision = '74ae1f164251'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
